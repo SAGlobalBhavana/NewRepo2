8189e8f0fe7bd779dcf0f65d636ad25ff60e3bcb
BhavSA <BhavanaaR@saglobal.com>
2025-03-19 11:19:55 +0530

    Added framework code

---
+++ PracticeTest/TestSuites/PM_Test.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+
+namespace PracticeTest.TestSuites
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static IWebDriver driver;
+        [TestMethod]
+        public void Method1()
+        {
+            // add the google 
+            // need to add thelogin part logic 
+              IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+          driver.Navigate().GoToUrl("https://www.Amazon.com");            
+        }
+		
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // add the google 
+            // need to add thelogin part logic 
+              IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+          driver.Navigate().GoToUrl("https://www.Amazon.com");            
+        }
+
+        [TestMethod]
+        public void SalesOrderCreation()
+        {
+            // add the google 
+            // need to add thelogin part logic 
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.Amazon.com");
+
+        }
+    }
+}
